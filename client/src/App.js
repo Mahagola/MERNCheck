9a5bcc7322e65b8428fc7a5e9e304a3c77d7f114
Mahagola <suryanshyadav885@gmail.com>
2024-04-15 04:18:56 +0530

    updated db version

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-const api_base = 'http://localhost:3000';
+const api_base = 'https://merncheck.onrender.com';
 
 function App() {
 	const [todos, setTodos] = useState([]);
